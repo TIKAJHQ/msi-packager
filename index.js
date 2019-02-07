b2833eeb14119f25b9a6f49879bef14cac9ed180
Madhurendra Sachan <m301@users.noreply.github.com>
2019-02-07 23:44:27 +0530

    fs write with callback

--- index.js
+++ index.js
@@ -30,10 +30,11 @@ module.exports = function(options, cb) {
 function writeXml(options, cb) {
   temp.open('msi-packager', function(err, info) {
     generateXml(options, function(err, xml) {
-      fs.write(info.fd, xml)
-      fs.close(info.fd, function (err) {
-        if (err) return cb(err)
-        cb(null, info.path)
+      fs.write(info.fd, xml,function(){
+        fs.close(info.fd, function (err) {
+          if (err) return cb(err)
+          cb(null, info.path)
+        })
       })
     })
   })
